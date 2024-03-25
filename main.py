79170d8ce2f185e76d14619730a56f66289d444d
Janin <jalumuputi@gmail.com>
2024-03-25 18:52:18 +0800

    added some process on main.py(variables for diag form) and moved the modl inside the form in homehtml

--- main.py
+++ main.py
@@ -5,7 +5,37 @@ from sklearn.tree import DecisionTreeClassifier
 app = Flask(__name__)
 
 @app.route("/",methods=["POST","GET"])
-def home():
+def home(): 
+    varfever = 1
+    varcough = 1
+    
+    vardifBr = 1
+    vargender = 1
+    varbp = 3
+    varchol = 1
+    if request.method == "POST":
+        
+        fever = request.form["fever"]
+        cough = request.form["cough"]
+        fatigue = request.form["fatigue"]
+        difBr = request.form["difBr"]
+        age = request.form["age"]
+        gender = request.form["gender"]
+        bp = request.form["bp"]
+        chol = request.form["chol"]
+   
+    if(fever=="Yes"):
+         varfever = 1
+    elif(cough=="Yes"):
+        varcough = 1
+    elif(fatigue=="Yes"):
+        varfatigue = 1  
+    elif(difBr=="Yes"):
+        vardifBr =1 
+    elif(gender=="Female" or gender=="female"):  
+        vargender = 1
+    elif(gender=="Male" or gender=="male"): 
+        vargender = 0
     data = pd.read_csv('data.csv')
 
     X = data.drop(columns=['Desease'])
@@ -14,29 +44,8 @@ def home():
     model = DecisionTreeClassifier()
 
     model.fit(X.values, y)
-    
-    # fever = request.form["fever"]
-    # cough = request.form["cough"]
-    # fatigue = request.form["fatigue"]
-    # difBr = request.form["difBr"]
-    # age = request.form["age"]
-    # gender = request.form["gender"]
-    # bp = request.form["bp"]
-    # chol = request.form["chol"]
-    
-   
-    
-    varfever = 1
-    varcough = 1
-    varfatigue = 0
-    vardifBr = 1
-    varage = 20
-    vargender = 1
-    varbp = 3
-    varchol = 1
-    
 
-    diagnosis = model.predict([[ varfever,varcough, varfatigue, vardifBr, varage, vargender, varbp, varchol]])
+    diagnosis = model.predict([[ varfever,varcough, varfatigue, vardifBr, age, vargender, varbp, varchol]])
 
     return render_template("home.html", diagnosis=diagnosis)
 
